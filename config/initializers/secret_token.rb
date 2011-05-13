619ecf0167b79075f1780e2835d9205238ab9542
flipito <durandfre@gmail.com>
2011-05-13 21:34:05 +0200

    other

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flipito::Application.config.secret_token = 'a2628204182deedf64b1d97c94031ddad65c57c9bf8e0cfcbe5afb7e84d46c3d57bcdf426d8e6835df3871d3299d8e2a5d961dca90f4a7970e9d5ff01970546a'
